7931d5c29f79520de11d7c4ad2377c17d40c0b4f
Zeyad Etman <zeyadetman@gmail.com>
2020-05-08 04:58:15 +0200

    first commit

--- main.js
+++ main.js
@@ -5,43 +5,47 @@ const getRandomInt = (min, max) => {
 };
 
 module.exports = async (client, config) => {
-  return new Promise(async (resolve) => {
-    const {
-      hashtag,
-      usersCountToFollow,
-      likesCountToLike,
-      comment,
-      mode,
-    } = config;
+  return new Promise(async (resolve, reject) => {
+    try {
+      const {
+        hashtag,
+        usersCountToFollow,
+        likesCountToLike,
+        comment,
+        mode,
+      } = config;
 
-    const photos = await client.getPhotosByHashtag({ hashtag });
-    const dataList = await photos.hashtag.edge_hashtag_to_media.edges.map(
-      (med) => (mode === "follow" ? med.node.owner.id : med.node.id)
-    );
+      const photos = await client.getPhotosByHashtag({ hashtag });
+      const dataList = await photos.hashtag.edge_hashtag_to_media.edges.map(
+        (med) => (mode === "follow" ? med.node.owner.id : med.node.id)
+      );
 
-    if (mode === "follow") {
-      let count = 0;
-      dataList.slice(0, usersCountToFollow).forEach(async (userId) => {
-        (async (userId) => {
-          setTimeout(async () => {
-            await client.follow({ userId });
-            if (count === usersCountToFollow - 1) resolve(true);
-            else count++;
-          }, getRandomInt(1000, 20000));
-        })(userId);
-      });
-    } else {
-      let count = 0;
-      dataList.slice(0, likesCountToLike).forEach(async (mediaId) => {
-        (async (userId) => {
-          setTimeout(async () => {
-            await client.like({ mediaId });
-            if (comment) await client.addComment({ mediaId, text: comment });
-            if (count === likesCountToLike - 1) resolve(true);
-            else count++;
-          }, getRandomInt(1000, 20000));
-        })(userId);
-      });
+      if (mode === "follow") {
+        let count = 0;
+        dataList.slice(0, usersCountToFollow).forEach(async (userId) => {
+          (async (userId) => {
+            setTimeout(async () => {
+              await client.follow({ userId });
+              if (count === usersCountToFollow - 1) resolve(true);
+              else count++;
+            }, getRandomInt(1000, 20000));
+          })(userId);
+        });
+      } else {
+        let count = 0;
+        dataList.slice(0, likesCountToLike).forEach(async (mediaId) => {
+          (async (userId) => {
+            setTimeout(async () => {
+              await client.like({ mediaId });
+              if (comment) await client.addComment({ mediaId, text: comment });
+              if (count === likesCountToLike - 1) resolve(true);
+              else count++;
+            }, getRandomInt(1000, 20000));
+          })(userId);
+        });
+      }
+    } catch (err) {
+      reject(err);
     }
   });
 };
